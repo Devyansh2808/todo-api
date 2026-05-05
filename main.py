10a2fee73de72ad4fb63c177ea06f3f278a8f61b
Devyansh <Devyanshraj1511@gmail.com>
2026-05-05 01:38:39 -0400

    addPOST /todos route

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+from models import *
+from datetime import datetime
+
+todos = {}
+next_id = 1
+
 app = FastAPI()
+
 @app.get("/")
 def read_root():
     return {"message": "hello"}
+
+@app.post("/todos")
+def create_todo(todo: TodoCreate):
+    global next_id
+    todo_item = TodoItem(**todo.model_dump(), id=next_id, created_at=datetime.now())
+    todos[next_id] = todo_item
+    next_id += 1
+    return todo_item
